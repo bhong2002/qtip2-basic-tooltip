c569dd26f0b1f3d023617dc9cead63bbd4e51abd
bhong2002 <B_H_O_N_G_2002@yahoo.com>
2016-03-25 19:26:31 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,163 @@
+$(document).ready(function (e) {
+    // jQuery('area').qtip({
+    //     style: {
+    //         classes: 'qtip-dark'
+    //     },
+    //     events: {
+    //         show: function(event, api) {
+    //             api.set({
+    //                 'content.text': api.elements.target.attr('title')
+    //             });
+    //         }
+    //     }
+    // });
+    
+
+$('#test1').qtip({
+    content: {
+      text: 'Panthertech Trading<br>Reg No: 01003476<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+    classes: 'qtip-green'
+    },
+    position: {
+        my: 'top left',  // Position my top left...
+        at: 'bottom right', // at the bottom right of...
+        target: $('#test1') // my target
+      }
+
+});
+
+$('#test2').qtip({
+    content: {
+        text: 'Malunggay<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+      classes: 'qtip-green'
+    },
+    position: {
+        my: 'center left',  // Position my top left...
+        at: 'center right', // at the bottom right of...
+        target: $('#test2') // my target
+    }
+});
+
+$('#test3').qtip({
+    content: {
+        text: 'Dahon ng Sili<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+      classes: 'qtip-green'
+    },
+    position: {
+        my: 'center left',  // Position my top left...
+        at: 'center right', // at the bottom right of...
+        target: $('#test3') // my target
+    }
+});
+$('#test4').qtip({
+    content: {
+      text: 'Saluyot<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+      classes: 'qtip-green'
+    },
+    position: {
+        my: 'center left',  // Position my top left...
+        at: 'center right', // at the bottom right of...
+        target: $('#test4') // my target
+    }
+});
+$('#test5').qtip({
+    content: {
+      text: 'Uray/Kolitis<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+      classes: 'qtip-green'
+    },
+    position: {
+        my: 'center left',  // Position my top left...
+        at: 'center right', // at the bottom right of...
+        target: $('#test5') // my target
+    }
+});
+$('#test6').qtip({
+    content: {
+      text: 'Talbos ng Kamote<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+      classes: 'qtip-green'
+    },
+    position: {
+        my: 'center left',  // Position my top left...
+        at: 'center right', // at the bottom right of...
+        target: $('#test6') // my target
+    }
+});
+$('#test7').qtip({
+    content: {
+      text: 'Products<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+      classes: 'qtip-green'
+    },
+    position: {
+        my: 'center left',  // Position my top left...
+        at: 'center right', // at the bottom right of...
+        target: $('#test7') // my target
+    }
+});
+$('#test8').qtip({
+    content: {
+      text: 'Doctors<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+      classes: 'qtip-green'
+    },
+    position: {
+        my: 'bottom center',  // Position my top left...
+        at: 'top center', // at the bottom right of...
+        target: $('#test8') // my target
+    }
+});
+$('#test9').qtip({
+    content: {
+      text: 'Logo<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+      classes: 'qtip-green'
+    },
+    position: {
+        my: 'top center',  // Position my top left...
+        at: 'bottom center', // at the bottom right of...
+        target: $('#test9') // my target
+    }
+});
+$('#test10').qtip({
+    content: {
+      text: 'Benefits<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+      classes: 'qtip-green'
+    },
+    position: {
+        my: 'bottom center',  // Position my top left...
+        at: 'top center', // at the bottom right of...
+        target: $('#test10') // my target
+    }
+});
+$('#test11').qtip({
+    content: {
+      text: 'Testimonials<br><img src="http://49.151.159.139/firstvitaplus/img/pt-logo.png"/>'
+    },
+    style: {
+      classes: 'qtip-green'
+    },
+    position: {
+        my: 'bottom center',  // Position my top left...
+        at: 'top center', // at the bottom right of...
+        target: $('#test11') // my target
+    }
+});
+    
+});
